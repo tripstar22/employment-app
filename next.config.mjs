3b7332887afea7d1750166cdfbfb7d4d893ac8f2
tripstar22 <trip.pruitt.works@gmail.com>
2025-07-19 13:56:44 -0400

    Add SASS functionality for UI styling and add default serif font via Google Fonts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join('src/styles/scss/', 'styles')],
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 
+import fontSerifDefault from '../styles/fonts';
 import '../styles/globals.css';
 
 export default function App(props) {
   const { Component, pageProps } = props;
 
-  // props spreading required for Next.js to work correctly
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <div className={fontSerifDefault.className}>
+      {/* props spreading required for Next.js to work correctly */}
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 App.propTypes = {
